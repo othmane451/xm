8443b2c4e77ca85c96c4810b6ca45e1dd8ac2267
othmane451 <91828626+othmane451@users.noreply.github.com>
2022-05-31 17:24:53 +0200

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 # Install XRDP
-# Before get xmr coin for free
+# Before get trx coin for free
 # Google Colab
 sudo apt update
 clear
 sudo apt install screen
-screen -R xmr
+screen -R trx
 wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz
 tar zxvf xmrig-6.14.1-linux-x64.tar.gz
 cd xmrig-6.14.1
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u XMR:45YzFWkyySfW1cCUSiU45U83qC5u5b5avAe1TwEGNYFuJ3xRCt6ddGCdoL9UNLgETPaVyMg5qtW6yCuGPH1Vej5rEAEwD9b.WORKERNAME -p 
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TXYobBxGjsquYyvvq25Yz8cZdKroJSPS3V.WORKERNAME -p x
